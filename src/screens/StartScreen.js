0ee64fd153eba87b3c18967fd0ccaf2e360847b1
oriyahazan <oriyaha.ac.sce.ac.il>
2025-01-04 20:41:27 +0200

    Remove buttons from the start screen

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -25,26 +25,6 @@ export default function StartScreen({ navigation }) {
         הרשמה
       </Button>
 
-      <Button
-        mode="outlined"
-        onPress={() => navigation.navigate('EnrichmentContent')}
-      >
-        תכני העשרה
-      </Button>
-
-      <Button
-        mode="outlined"
-        onPress={() => navigation.navigate('HomeScreenManager')}
-      >
-דף בית מנהל      </Button>
-
-      <Button
-        mode="outlined"
-        onPress={() => navigation.navigate('HomeScreen')}
-      >
-דף בית
-      </Button>
-
     </Background>
   )
 }
