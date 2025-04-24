bee3be30c8849f8a50cf92304c4bf7b0fb0e0524
oriyahazan <oriyaha.ac.sce.ac.il>
2025-04-24 13:29:36 +0300

    Additional enrichment content

--- src/helpers/category.js
+++ src/helpers/category.js
@@ -1,161 +1,147 @@
 export const categories = [
-    {
-      icon: '🌬️',
-      category: 'mental_health',
-      titleHe: 'תרגול נשימה והרפיה',
-      titleEn: 'תרגולים שיעזרו לך להירגע ולשחרר מתח נפשי',
-      image: require('../assets/1.jpeg'),
-      color: ['#FFF5F5'],
-      content: [
-        { type: 'text', value: 'בואו ללמוד כיצד לבצע תרגול נשימה פשוט שמסייע בהרפיה של הגוף והנפש' },
-        { type: 'video', value: 'https://www.youtube.com/embed/bnCuQRO5xjE' },
-        { type: 'text', value: 'בנוסף, תוכלו לצפות בשיטת 4-7-8, שיטה ייחודית לנשימה מודעת ונכונה' },
-        { type: 'video', value: 'https://www.youtube.com/embed/n8luVodumAE' },
-      ]
-    },
-    {
-      icon: '💪',
-      category: "self_confidence",
-      titleHe: 'העצמה אישית והתמודדות עם פחדים',
-      titleEn: 'כלים להתמודדות עם פחדים, חרדות והעצמת הביטחון העצמי',
-      image: require('../assets/2.jpeg'),
-      color: ['#F7FAFC'],
-      content: [
-        { type: 'text', value: 'תרגיל "דמיון מודרך" להתמודדות עם פחדים' },
-        { type: 'video', value: 'https://www.youtube.com/embed/SYrpH4CnfKQ' },
-        { type: 'text', value: 'טיפים לשיפור הביטחון האישי'}, 
-        { type: 'website', value: 'https://www.cbtisrael.co.il/2020/01/29/10-טיפים-שיעזרו-לך-להתגבר-על-חוסר-ביטחון/' },
+  {
+    icon: '🌬️', 
+    category: 'mental_health',
+    titleHe: 'תרגול נשימה והרפיה',
+    titleEn: 'תרגולים שיעזרו לך להירגע ולשחרר מתח נפשי',
+    image: require('../assets/enrichmentContent/breathing.jpeg'),
+    color: ['#FFF5F5'],
+    content: [
+      { type: 'text', value: 'בואו ללמוד כיצד לבצע תרגול נשימה פשוט שמסייע בהרפיה של הגוף והנפש' },
+      { type: 'video', value: 'https://www.youtube.com/embed/bnCuQRO5xjE' },
+      { type: 'text', value: 'בנוסף, תוכלו לצפות בשיטת 4-7-8, שיטה ייחודית לנשימה מודעת ונכונה' },
+      { type: 'video', value: 'https://www.youtube.com/embed/n8luVodumAE' },
+    ]
+  },
+  {
+    icon: '😔', 
+    category: "depression",
+    titleHe: 'התמודדות עם דיכאון',
+    titleEn: 'כלים וטכניקות להתמודדות עם דיכאון, להקל על ההתמודדות היומיומית',
+    image: require('../assets/enrichmentContent/Depression.jpeg'),
+    color: ['#f1e9f5'],
+    content: [
+      { type: 'text', value: 'טיפול בדיכאון' },
+      { type: 'video', value: 'https://www.youtube.com/watch?v=TTHOjqIRQ34' },
+      { type: 'text', value: 'תסמינים ודרכי טיפול' },
+      { type: 'video', value: 'https://www.grunhaus.co.il/טיפול-בדיכאון/' },
+    ]
+  },
 
-      ]
-    },
-    {
-        icon: '🌿',
-        category: 'trauma',
-        titleHe: 'התמודדות עם טראומה',
-        titleEn: 'כלים וטכניקות להתמודדות עם טראומה, להקל על ההתמודדות היומיומית',
-        image: require('../assets/3.jpeg'),
-        color: ['#f1e9f5'],
-        content: [
-          { type: 'text', value: 'תרגילי יוגה לעידוד תחושת רוגע' },
-          { type: 'video', value: 'https://www.youtube.com/embed/8TuRYV71Rgo' },
-          { type: 'text', value: 'איך לדעת אם יש לי טראומה?' },
-          { type: 'video', value: 'https://www.youtube.com/watch?v=7I6Pl1qG4IQ' },
-          { type: 'text', value: 'מרגיעים את הטראומה' },
-          { type: 'website', value: 'https://www.clalitmashlima.co.il/טראומה-רפואה-משלימה/' },
-        ]
-      },
-  
-    {
-      icon: '🌿',
-      category: "depression",
-      titleHe: 'התמודדות עם דיכאון',
-      titleEn: 'כלים וטכניקות להתמודדות עם דיכאון, להקל על ההתמודדות היומיומית',
-      image: require('../assets/3.jpeg'),
+  {
+      icon: '🧍‍♂️',
+      category: "loneliness",
+      titleHe: 'התמודדות עם בדידות',
+      titleEn: 'כלים וטכניקות להתמודדות עם בדידות, להקל על ההתמודדות היומיומית',
+      image: require('../assets/enrichmentContent/loneliness.jpeg'),
       color: ['#f1e9f5'],
       content: [
-        { type: 'text', value: 'טיפול בדיכאון' },
-        { type: 'video', value: 'https://www.youtube.com/watch?v=TTHOjqIRQ34' },
-        { type: 'text', value: 'תסמינים ודרכי טיפול' },
-        { type: 'video', value: 'https://www.grunhaus.co.il/טיפול-בדיכאון/' },
-
+          { type: 'text', value: 'התדר - פודקאסט' },
+          { type: 'website', value: 'https://open.spotify.com/show/3gQhtfsb6DbGEI1ihgI9fh' },
+          { type: 'text', value: 'איך להתמודד עם בדידות' },
+          { type: 'website', value: 'https://dror-psy.co.il/בעיות-רגשיות/בדידות/' },
       ]
-    },
+  },
+  {
+    icon: '💪', 
+    category: "self_confidence",
+    titleHe: 'העצמה אישית והתמודדות עם פחדים',
+    titleEn: 'כלים להתמודדות עם פחדים, חרדות והעצמת הביטחון העצמי',
+    image: require('../assets/enrichmentContent/PersonalEmpowerment.jpeg'),
+    color: ['#F7FAFC'],
+    content: [
+      { type: 'text', value: 'תרגיל "דמיון מודרך" להתמודדות עם פחדים' },
+      { type: 'video', value: 'https://www.youtube.com/embed/SYrpH4CnfKQ' },
+      { type: 'text', value: 'טיפים לשיפור הביטחון האישי'}, 
+      { type: 'website', value: 'https://www.cbtisrael.co.il/2020/01/29/10-טיפים-שיעזרו-לך-להתגבר-על-חוסר-ביטחון/' },
+    ]
+  },
+  {
+      icon: '😴', 
+      category: "sleep_issues",
+      titleHe: 'התמודדות עם בעיות שינה',
+      titleEn: 'כלים וטכניקות להתמודדות עם בעיות שינה',
+      image: require('../assets/enrichmentContent/Sleep.jpeg'),
+      color: ['#f1e9f5'],
+      content: [
+        { type: 'text', value: 'מוזיקה לפני שינה' },
+        { type: 'website', value: 'https://www.youtube.com/watch?v=vn_PmKgCpDs' },
+        { type: 'text', value: 'sleep with me' },
+        { type: 'website', value: 'https://www.sleepwithmepodcast.com' },
+      ]
+  },
 
-    {
-        icon: '🌿',
-        category: "loneliness",
-        titleHe: 'התמודדות עם בדידות',
-        titleEn: 'כלים וטכניקות להתמודדות עם בדידות, להקל על ההתמודדות היומיומית',
-        image: require('../assets/3.jpeg'),
-        color: ['#f1e9f5'],
-        content: [
-            { type: 'text', value: 'התדר - פודקאסט' },
-            { type: 'website', value: 'https://open.spotify.com/show/3gQhtfsb6DbGEI1ihgI9fh' },
-            { type: 'text', value: 'איך להתמודד עם בדידות' },
-            { type: 'website', value: 'https://dror-psy.co.il/בעיות-רגשיות/בדידות/' },
+  {
+      icon: '😰', 
+      category: "anxiety",
+      titleHe: 'התמודדות עם חרדה',
+      titleEn: 'כלים וטכניקות להתמודדות עם חרדה',
+      image: require('../assets/enrichmentContent/anxiety.jpeg'),
+      color: ['#f1e9f5'],
+      content: [
+        { type: 'text', value: 'מדיטציה להרגעת חרדה ולחצים' },
+        { type: 'video', value: 'https://www.youtube.com/watch?v=JMEAm4Njeq8' },
+        { type: 'text', value: 'דימיון מודרך' },
+        { type: 'video', value: 'https://www.youtube.com/watch?v=OZmEt7auY0k' },
+      ]
+  },
+  {
+    icon: '🧠', 
+    category: 'trauma',
+    titleHe: 'התמודדות עם טראומה',
+    titleEn: 'כלים וטכניקות להתמודדות עם טראומה, להקל על ההתמודדות היומיומית',
+    image: require('../assets/enrichmentContent/trauma.jpeg'),
+    color: ['#f1e9f5'],
+    content: [
+      { type: 'text', value: 'תרגילי יוגה לעידוד תחושת רוגע' },
+      { type: 'video', value: 'https://www.youtube.com/embed/8TuRYV71Rgo' },
+      { type: 'text', value: 'איך לדעת אם יש לי טראומה?' },
+      { type: 'video', value: 'https://www.youtube.com/watch?v=7I6Pl1qG4IQ' },
+      { type: 'text', value: 'מרגיעים את הטראומה' },
+      { type: 'website', value: 'https://www.clalitmashlima.co.il/טראומה-רפואה-משלימה/' },
+    ]
+  },
+  {
+    icon: '🧘‍♀️', 
+    titleHe: 'מדיטציה ומיינדפולנס',
+    category: "motivation",
+    titleEn: 'תרגולי מדיטציה ומיינדפולנס לשיפור הרוגע הפנימי',
+    image: require('../assets/enrichmentContent/meditation.jpeg'),
+    color: ['#FFF5F5'],
+    content: [
+      { type: 'text', value: 'מדיטציית מיינדפולנס של 5 דקות - לרגיעה ושלווה מיידיים' },
+      { type: 'video', value: 'https://www.youtube.com/embed/TENB02x_cig' },
+      { type: 'text', value: 'סרטון מוטיבציה' },
+      { type: 'video', value: 'https://www.youtube.com/watch?v=fLeJJPxua3E' },
+    ]
+  },
 
-        ]
-    },
-  
-    {
-        icon: '🌿',
-        category: "sleep_issues",
-        titleHe: 'התמודדות עם בעיות שינה',
-        titleEn: 'כלים וטכניקות להתמודדות עם בעיות שינה',
-        image: require('../assets/3.jpeg'),
-        color: ['#f1e9f5'],
-        content: [
-          { type: 'text', value: 'מוזיקה לפני שינה' },
-          { type: 'website', value: 'https://www.youtube.com/watch?v=vn_PmKgCpDs' },
-          { type: 'text', value: 'sleep with me' },
-          { type: 'website', value: 'https://www.sleepwithmepodcast.com' },
-  
-        ]
-    },
-
-    {
-        icon: '🌿',
-        category: "anxiety",
-        titleHe: 'התמודדות עם חרדה',
-        titleEn: 'כלים וטכניקות להתמודדות עם חרדה',
-        image: require('../assets/3.jpeg'),
-        color: ['#f1e9f5'],
-        content: [
-          { type: 'text', value: 'מדיטציה להרגעת חרדה ולחצים' },
-          { type: 'video', value: 'https://www.youtube.com/watch?v=JMEAm4Njeq8' },
-          { type: 'text', value: 'דימיון מודרך' },
-          { type: 'video', value: 'https://www.youtube.com/watch?v=OZmEt7auY0k' },
-  
-        ]
-    },
-
-    {
-      icon: '🧘‍♀️',
-      titleHe: 'מדיטציה ומיינדפולנס',
-      category: "motivation",
-      titleEn: 'תרגולי מדיטציה ומיינדפולנס לשיפור הרוגע הפנימי',
-      image: require('../assets/4.jpeg'),
+  {
+      icon: '🎭', 
+      titleHe: 'ויסות רגשי',
+      category: "emotional_regulation",
+      titleEn: 'תרגולי מדיטציה ומיינדפולנס לשיפור הויסות הרגשי',
+      image: require('../assets/enrichmentContent/EmotionalRegulation.jpeg'),
       color: ['#FFF5F5'],
       content: [
-        { type: 'text', value: 'מדיטציית מיינדפולנס של 5 דקות - לרגיעה ושלווה מיידיים' },
-        { type: 'video', value: 'https://www.youtube.com/embed/TENB02x_cig' },
-        { type: 'text', value: 'סרטון מוטיבציה' },
-        { type: 'video', value: 'https://www.youtube.com/watch?v=fLeJJPxua3E' },
-
+        { type: 'text', value: 'שלושה טיפים להתמדודות עם ויסות רגשי' },
+        { type: 'video', value: 'https://www.youtube.com/watch?v=SCbifUfr-js' },
+        { type: 'text', value: 'ויסות רגשי ורגשות עם דר יעל' },
+        { type: 'video', value: 'https://www.youtube.com/watch?v=t7HJhJhDNXA' },
       ]
     },
-
     {
-        icon: '🧘‍♀️',
-        titleHe: 'ויסות רגשי',
-        category: "emotional_regulation",
-        titleEn: 'תרגולי מדיטציה ומיינדפולנס לשיפור הויסות הרגשי',
-        image: require('../assets/4.jpeg'),
-        color: ['#FFF5F5'],
-        content: [
-          { type: 'text', value: 'שלושה טיפים להתמדודות עם ויסות רגשי' },
-          { type: 'video', value: 'https://www.youtube.com/watch?v=SCbifUfr-js' },
-          { type: 'text', value: 'ויסות רגשי ורגשות עם דר יעל' },
-          { type: 'video', value: 'https://www.youtube.com/watch?v=t7HJhJhDNXA' },
-  
-        ]
-      },
-      {
-        icon: '🧘‍♀️',
-        titleHe: 'תמיכה קבוצתית',
-        category: "social_support",
-        titleEn: 'איך לנהל תמיכה קבוצתית',
-        image: require('../assets/4.jpeg'),
-        color: ['#FFF5F5'],
-        content: [
-          { type: 'text', value: 'החשיבות של קבוצות תמיכה' },
-          { type: 'website', value: 'https://nefeshh.org/החשיבות-של-קבוצות-תמיכה-למתמודדי-נפש/' },
-          { type: 'text', value: 'כוחה של קבוצה' },
-          { type: 'website', value: 'https://www.guy.org.il/group-therapy/' },
-  
-        ]
-      },
-
-  
-  ];
-  
+      icon: '🤝', 
+      titleHe: 'תמיכה קבוצתית',
+      category: "social_support",
+      titleEn: 'איך לנהל תמיכה קבוצתית',
+      image: require('../assets/enrichmentContent/support.jpeg'),
+      color: ['#FFF5F5'],
+      content: [
+        { type: 'text', value: 'החשיבות של קבוצות תמיכה' },
+        { type: 'website', value: 'https://nefeshh.org/החשיבות-של-קבוצות-תמיכה-למתמודדי-נפש/' },
+        { type: 'text', value: 'כוחה של קבוצה' },
+        { type: 'website', value: 'https://www.guy.org.il/group-therapy/' },
+      ]
+    },
+];
